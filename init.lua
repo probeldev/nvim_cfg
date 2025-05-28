116e5be5af35179b35ea26feac9c9f76c3a4e73c
Sergey Protko <sergey.protko@yandex.ru>
2025-05-28 13:14:02 +0300

    migrate 0.11.1

--- init.lua
+++ init.lua
@@ -1,130 +1,130 @@
-	-- Импорт пользовательских горячих клавиш
-	vim.cmd('source ~/.config/nvim/hotkey.vim')
+-- Импорт пользовательских горячих клавиш
+vim.cmd('source ~/.config/nvim/hotkey.vim')
 
-	-- Инициализация менеджера плагинов (vim-plug)
-	local Plug = vim.fn['plug#begin']()
-	vim.fn['plug#']('nvim-tree/nvim-tree.lua')
-	vim.fn['plug#']('nvim-lua/plenary.nvim')
-	vim.fn['plug#']('nvim-tree/nvim-web-devicons')
-	vim.fn['plug#']('MunifTanjim/nui.nvim')
-	vim.fn['plug#']('L3MON4D3/LuaSnip')
-	vim.fn['plug#']('lewis6991/gitsigns.nvim')
-	vim.fn['plug#']('romgrk/barbar.nvim')
-	vim.fn['plug#']('ryanoasis/vim-devicons')
-	vim.fn['plug#']('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.6' })
-	vim.fn['plug#']('tomtom/tcomment_vim')
-	vim.fn['plug#']('akinsho/toggleterm.nvim', { ['tag'] = '*' })
-	vim.fn['plug#']('tpope/vim-dadbod')
-	vim.fn['plug#']('kristijanhusak/vim-dadbod-ui')
-	vim.fn['plug#']('nvim-lualine/lualine.nvim')
-	vim.fn['plug#']('stevearc/conform.nvim')
-	vim.fn['plug#']('projekt0n/github-nvim-theme')
-	vim.fn['plug#']('dstein64/nvim-scrollview')
-	vim.fn['plug#']('SmiteshP/nvim-navic')
-	vim.fn['plug#']('utilyre/barbecue.nvim')
-	vim.fn['plug#']('sontungexpt/stcursorword')
-	vim.fn['plug#']('David-Kunz/gen.nvim')
-	vim.fn['plug#']('karb94/neoscroll.nvim')
-	vim.fn['plug#']('tanvirtin/vgit.nvim')
-	vim.fn['plug#end']()
+-- Инициализация менеджера плагинов (vim-plug)
+local Plug = vim.fn['plug#begin']()
+vim.fn['plug#']('nvim-tree/nvim-tree.lua')
+vim.fn['plug#']('nvim-lua/plenary.nvim')
+vim.fn['plug#']('nvim-tree/nvim-web-devicons')
+vim.fn['plug#']('MunifTanjim/nui.nvim')
+vim.fn['plug#']('L3MON4D3/LuaSnip')
+vim.fn['plug#']('lewis6991/gitsigns.nvim')
+vim.fn['plug#']('romgrk/barbar.nvim')
+vim.fn['plug#']('ryanoasis/vim-devicons')
+vim.fn['plug#']('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.6' })
+vim.fn['plug#']('tomtom/tcomment_vim')
+vim.fn['plug#']('akinsho/toggleterm.nvim', { ['tag'] = '*' })
+vim.fn['plug#']('tpope/vim-dadbod')
+vim.fn['plug#']('kristijanhusak/vim-dadbod-ui')
+vim.fn['plug#']('nvim-lualine/lualine.nvim')
+vim.fn['plug#']('stevearc/conform.nvim')
+vim.fn['plug#']('projekt0n/github-nvim-theme')
+vim.fn['plug#']('dstein64/nvim-scrollview')
+vim.fn['plug#']('SmiteshP/nvim-navic')
+vim.fn['plug#']('utilyre/barbecue.nvim')
+vim.fn['plug#']('sontungexpt/stcursorword')
+vim.fn['plug#']('David-Kunz/gen.nvim')
+vim.fn['plug#']('karb94/neoscroll.nvim')
+vim.fn['plug#']('tanvirtin/vgit.nvim')
+vim.fn['plug#end']()
 
-	-- Базовые настройки редактора
-	vim.opt.mouse = 'a'                          -- Включить мышь
-	vim.opt.number = true                        -- Показывать номера строк
-	vim.opt.tabstop = 4                          -- Размер табуляции
-	vim.opt.shiftwidth = 4                       -- Размер отступа
-	vim.opt.list = true                          -- Показывать скрытые символы
-	vim.opt.listchars = 'tab:|—,trail:⋅,nbsp:⋅'  -- Символы для табуляции и пробелов
-	vim.opt.cursorline = true                    -- Подсветка текущей строки
-	vim.opt.clipboard = 'unnamedplus'            -- Использовать системный буфер обмена
+-- Базовые настройки редактора
+vim.opt.mouse = 'a'                          -- Включить мышь
+vim.opt.number = true                        -- Показывать номера строк
+vim.opt.tabstop = 4                          -- Размер табуляции
+vim.opt.shiftwidth = 4                       -- Размер отступа
+vim.opt.list = true                          -- Показывать скрытые символы
+vim.opt.listchars = 'tab:|—,trail:⋅,nbsp:⋅'  -- Символы для табуляции и пробелов
+vim.opt.cursorline = true                    -- Подсветка текущей строки
+vim.opt.clipboard = 'unnamedplus'            -- Использовать системный буфер обмена
 
-	-- Цветовая схема
-	vim.opt.background = 'light'
-	vim.cmd('colorscheme github_light')
+-- Цветовая схема
+vim.opt.background = 'light'
+vim.cmd('colorscheme github_light')
 
-	-- Настройка плагинов
-	require('toggleterm').setup()
-	require('vgit').setup()
-	require('lualine').setup()
+-- Настройка плагинов
+require('toggleterm').setup()
+require('vgit').setup()
+require('lualine').setup()
 
-	-- Форматирование с conform.nvim
-	require('conform').setup({
-	  formatters_by_ft = {
+-- Форматирование с conform.nvim
+require('conform').setup({
+	formatters_by_ft = {
 		sh = { 'shfmt' },
-	  },
-	  format_on_save = {
+	},
+	format_on_save = {
 		timeout_ms = 500,
 		lsp_fallback = true,
-	  },
-	})
+	},
+})
 
 
 
-	-- Перехватчик для textDocument/publishDiagnostics
-	vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-	  vim.lsp.diagnostic.on_publish_diagnostics, {
+-- Перехватчик для textDocument/publishDiagnostics
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+	vim.lsp.diagnostic.on_publish_diagnostics, {
 		virtual_text = {
-		  prefix = '●',
+			prefix = '●',
 		},
 		signs = true,
 		update_in_insert = true,
 		severity_sort = true,
-	  }
-	)
+	}
+)
 
 
 
-	-- Включение LSP серверов
-	vim.lsp.enable({ 'gopls', 'golangci_lint_ls' })
+-- Включение LSP серверов
+vim.lsp.enable({ 'gopls', 'golangci_lint_ls' })
 
-	-- Настройка диагностики (показ всех уровней)
-	vim.diagnostic.config({
-	  virtual_lines =  true,
-	  virtual_text = false,
-	  signs = true,
-	  update_in_insert = true,
-	  severity_sort = true,
-	})
+-- Настройка диагностики (показ всех уровней)
+vim.diagnostic.config({
+	virtual_lines =  true,
+	virtual_text = false,
+	signs = true,
+	update_in_insert = true,
+	severity_sort = true,
+})
 
 
 
-	vim.opt.completeopt = { 'menuone', 'noselect', 'noinsert' }
-	-- Автоматическое включение автодополнения для LSP
-	vim.api.nvim_create_autocmd('LspAttach', {
-	  callback = function(ev)
+vim.opt.completeopt = { 'menuone', 'noselect', 'noinsert' }
+-- Автоматическое включение автодополнения для LSP
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
 
 		if client.server_capabilities.completionProvider then
-		  -- Только буквы (латиница + кириллица + возможно другие языки)
-		  client.server_capabilities.completionProvider.triggerCharacters = {
-			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-			'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-			'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-			'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л',
-			'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш',
-			'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',
-			'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л',
-			'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш',
-			'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я',
-			'.',
-		  }
+			-- Только буквы (латиница + кириллица + возможно другие языки)
+			client.server_capabilities.completionProvider.triggerCharacters = {
+				'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+				'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+				'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+				'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+				'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л',
+				'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш',
+				'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',
+				'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л',
+				'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш',
+				'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я',
+				'.',
+			}
 		end
 
 		if client:supports_method('textDocument/completion') then
-		  vim.lsp.completion.enable(true, client.id, ev.buf, { 
-		autotrigger = true,
-		convert = function(item)
-			-- Опционально: можно преобразовывать элементы дополнения
-			return {
-			  abbr = item.label:gsub('%b()', ''),  -- Удаляем скобки для краткости
-			  word = item.insertText or item.label,  -- Используем insertText если есть
-			  kind = item.kind,                     -- Сохраняем тип
-			}
-		  end
-	  })
-    end
-  end,
+			vim.lsp.completion.enable(true, client.id, ev.buf, { 
+				autotrigger = true,
+				convert = function(item)
+					-- Опционально: можно преобразовывать элементы дополнения
+					return {
+						abbr = item.label:gsub('%b()', ''),  -- Удаляем скобки для краткости
+						word = item.insertText or item.label,  -- Используем insertText если есть
+						kind = item.kind,                     -- Сохраняем тип
+					}
+				end
+			})
+		end
+	end,
 })
 
 -- TODO:  move to hotkey
@@ -133,23 +133,23 @@ vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { no
 
 -- Настройка nvim-tree
 require('nvim-tree').setup({
-  diagnostics = {
-    enable = true,
-    show_on_dirs = true,
-  },
+	diagnostics = {
+		enable = true,
+		show_on_dirs = true,
+	},
 })
 
 -- Настройка barbar
 require('barbar').setup({
-  sidebar_filetypes = {
-    NvimTree = true,
-    undotree = {
-      text = 'undotree',
-      align = 'center',
-    },
-    ['neo-tree'] = { event = 'BufWipeout' },
-    Outline = { event = 'BufWinLeave', text = 'symbols-outline', align = 'right' },
-  },
+	sidebar_filetypes = {
+		NvimTree = true,
+		undotree = {
+			text = 'undotree',
+			align = 'center',
+		},
+		['neo-tree'] = { event = 'BufWipeout' },
+		Outline = { event = 'BufWinLeave', text = 'symbols-outline', align = 'right' },
+	},
 })
 
 -- Дополнительные настройки плагинов
