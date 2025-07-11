a70492c2b4d9ee606e006e9fbdd3981b472785bd
Sergey Protko <sergey.protko@yandex.ru>
2025-07-11 06:11:51 +0300

    add auto run goimports on save

--- nvim/lsp/gopls.lua
+++ nvim/lsp/gopls.lua
@@ -19,11 +19,19 @@ vim.lsp.config.gopls = {
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
-    -- Сохраняем текущую позицию курсора
+    -- Сохраняем оригинальное содержимое
+    local original_content = vim.api.nvim_buf_get_lines(0, 0, -1, false)
     local cursor_pos = vim.api.nvim_win_get_cursor(0)
-    -- Выполняем goimports
-    vim.cmd("%!goimports")
-    -- Восстанавливаем позицию курсора
-    vim.api.nvim_win_set_cursor(0, cursor_pos)
+
+    -- Пробуем выполнить goimports
+    local formatted = vim.fn.systemlist('goimports', original_content)
+    
+    -- Проверяем успешность выполнения (код возврата 0)
+    if vim.v.shell_error == 0 then
+      vim.api.nvim_buf_set_lines(0, 0, -1, false, formatted)
+      vim.api.nvim_win_set_cursor(0, cursor_pos)
+    else
+      vim.notify("goimports failed: " .. table.concat(formatted, "\n"), vim.log.levels.ERROR)
+    end
   end,
 })
