ef23a8d4cf83ca990a9ce3df35d94e4835e70afa
Sergey Protko <sergey.protko@yandex.ru>
2025-07-11 05:27:31 +0300

    add auto run goimports on save

--- nvim/lsp/gopls.lua
+++ nvim/lsp/gopls.lua
@@ -15,3 +15,15 @@ vim.lsp.config.gopls = {
     },
   },
 }
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    -- Сохраняем текущую позицию курсора
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    -- Выполняем goimports
+    vim.cmd("%!goimports")
+    -- Восстанавливаем позицию курсора
+    vim.api.nvim_win_set_cursor(0, cursor_pos)
+  end,
+})
