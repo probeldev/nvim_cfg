eb7c24ebe4ee232316c4784d2f96f83adc8bea7c
Sergey <sergey@MacBook-Air-Sergey.local>
2025-07-26 13:44:50 +0300

    update instal.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
-rm -rf ~/.config/nvim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+rm -rf ~/.config/nvim || echo 1
+mkdir -p ~/.config
 cp -r nvim ~/.config/nvim
